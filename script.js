5437215ae2447842d874c1274e7fb3089d39d99d
bsison-sdsu <bsison2390@sdsu.edu>
2026-03-24 19:58:30 -0700

    Desktop Semesters Dropdown
    I added desktop drowdown for semester menu. I also added smooth scrolling. Positioning for menu also was made so that dropdown works correctly.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const mobileMenu = document.querySelector('#mobile-menu');
+
 function showMobileLinks() {
     mobileMenu.classList.toggle('open');
 }
 
-const mobileMenu = document.querySelector('#mobile-menu');
+const semesterMenu = document.querySelector('.desktop_links .semester-hover');
+const semesterList = document.querySelector('.desktop_links .semester-hover .semester-dropdown');
+
+function showSemesterLinks() {
+    semesterList.classList.toggle('open');
+}
+
+semesterMenu.addEventListener('mouseenter', showSemesterLinks);
+semesterMenu.addEventListener('mouseleave', showSemesterLinks);
