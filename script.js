aa5232f65dd2f81605f91d69067ca763888e8f3c
bsison-sdsu <bsison2390@sdsu.edu>
2026-04-16 23:06:09 -0700

    Added Light/Dark Mode
    I added the light/dark mode feature. Included toggle and time feature so at around 6pm theme will automatically be dark mode if theme isn't toggled. Also added a local storage feature so that if theme is changed it will follow from page to page.

--- script.js
+++ script.js
@@ -27,3 +27,44 @@ for (let i = 0; i < mobileSemesterOption.length; i++) {
 }
 
 mobileSemesterMenu.addEventListener('click', showMobileSemesterLinks);
+
+//DARK MODE 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const htmlEl = document.documentElement;
+const savedTheme = localStorage.getItem('theme');
+
+function setTheme(isDark) {
+    if (isDark) {
+        htmlEl.setAttribute('data-theme', 'dark');
+        darkModeToggle.src = 'icons/clear_day_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        htmlEl.removeAttribute('data-theme');
+        darkModeToggle.src = 'icons/dark_mode_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg'
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+function shouldBeDark() {
+    const now = new Date();
+    const hour = now.getHours();
+    return hour >= 18;
+}
+
+if (savedTheme) {
+    setTheme(savedTheme === 'dark');
+} else {
+    setTheme(shouldBeDark());
+}
+
+setInterval(function(){
+    if (!localStorage.getItem('theme')) {
+        setTheme(shouldBeDark());
+    }
+}, 60000);
+
+darkModeToggle.addEventListener('click', function() {
+    const isDarkNow = htmlEl.getAttribute('data-theme') === 'dark';
+    setTheme(!isDarkNow);
+});
